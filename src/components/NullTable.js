3e10c6c635a81c2ede6315c096e5db4637b67843
Daniel Fleace <danielfleace15@gmail.com>
2022-02-22 21:33:17 -0500

    show sample user info on home screen

--- src/components/NullTable.js
+++ src/components/NullTable.js
@@ -1,7 +1,9 @@
 const NullTable = () => {    
   return(
     <div className="nulltable" >
-      Please login to track your data and display your previous 7 days!
+      Please login to track your data and display your previous 7 days!<br/><br/>
+      Try username: Daniel & password DanielF for sample data! 
+      
     </div>   
   )
 }
